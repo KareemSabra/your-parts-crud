81a30620b37dc765abf03879aac5587372482eb5
Kareem Sabra <63441894+KareemSabra@users.noreply.github.com>
2024-06-26 22:28:38 +0300

    Responsive navbar

--- components/Header/navbar.tsx
+++ components/Header/navbar.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Paths } from '@/routes/paths';
 import Typography from '../typography';
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className=" bg-opacity-70 backdrop-blur-md shadow-lg fixed w-full z-50 border-b-2 border-blue-900">
-      <div className="container mx-auto p-4 flex justify-between items-center 	">
-        <div>
-          <Link href="/">
-            <Typography variant="h2">To do list</Typography>
-          </Link>
-        </div>
-        <div className="space-x-4 flex">
-          {Paths.map((path, index) => (
-            <Link key={index} href={path.path}>
-              <Typography variant="h6">{path.label}</Typography>
+    <nav className="bg-opacity-70 backdrop-blur-md shadow-lg fixed w-full z-50 border-b-2 border-blue-900">
+      <div className="container mx-auto px-4 md:px-6 py-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <Link href="/">
+              <Typography variant="h2">To do list</Typography>
             </Link>
-          ))}
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-blue-900 focus:outline-none focus:text-blue-500"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          <div className="hidden md:flex space-x-4">
+            {Paths.map((path, index) => (
+              <Link key={index} href={path.path}>
+                <Typography variant="h6">{path.label}</Typography>
+              </Link>
+            ))}
+          </div>
         </div>
+        {isOpen && (
+          <div className="md:hidden mt-2">
+            {Paths.map((path, index) => (
+              <Link key={index} href={path.path}>
+                <Typography variant="h6" className="block py-2">
+                  {path.label}
+                </Typography>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </nav>
   );
