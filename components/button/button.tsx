6ec0c50c7dd3685a5210ee1e4722084c24975342
Kareem Sabra <63441894+KareemSabra@users.noreply.github.com>
2024-06-30 22:30:52 +0300

    update button for light mode

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -21,7 +21,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className="bg-blue-900 px-4 py-2 rounded-md hover:bg-blue-400 disabled:hover:bg-blue-900 disabled:cursor-not-allowed"
+      className="bg-blue-900 px-4 py-2 rounded-md hover:bg-blue-400 disabled:hover:bg-blue-900 disabled:cursor-not-allowed text-white"
       onClick={onClick}
       disabled={disabled || isSubmitting}
       type={type}
